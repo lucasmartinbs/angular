e7e452b5cfd2c5b20064cc4b5ed1eac5de209c89
lucasmartinbs <lucasmartinbs@gmail.com>
2015-05-22 10:52:14 -0300

    se agreco index y controller.js

---
+++ controller.js
@@ -0,0 +1,11 @@
+var app = angular.module("myapp",[]);
+
+app.controller("mycontroller",function($scope){
+	$scope.nombre= "lucasmartinbs";
+
+});
+
+app.controller("controller2",function($scope){
+	$scope.nombre= "angular controller";
+
+});
